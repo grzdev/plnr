f745a50d27a6155a2829d4551562e399d14507af
zay <damilolaoyeniyi13@gmail.com>
2022-10-15 21:31:54 +0100

    Persit data to local storage

--- components/input.js
+++ components/input.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { BsPlusLg } from "react-icons/bs"
 import { saveTodo } from '../redux/slices/todoSlice'
 
 export default function Input() {
-    const [ input, setInput ] = useState("")
+    const [ input, setInput ] = useState([], ()=>{
+        const localData = localStorage.getItem('input');
+        return localData ? JSON.parse(localData) : []
+    })
+    useEffect(()=>{
+        localStorage.setItem('input', JSON.stringify(input))
+    }, [input])
+
     const dispatch = useDispatch()
+
+    
+    const date = new Date();
+    const dateTimeInSeconds = Math.floor(date.getTime() / 1000);
 
     const addTodo = () => {
         dispatch(saveTodo({
             name : input,
             done: false,
-            id: Date.now()
+            id: dateTimeInSeconds
         }))
     }
   return (
