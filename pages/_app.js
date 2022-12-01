646d8d6b36c3fe1e6be54ddf61d8e88906c29177
zay <damilolaoyeniyi13@gmail.com>
2022-12-01 17:33:09 +0100

    Persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,42 @@
 
-import { Provider } from "react-redux"
-import store from "../redux/store"
-import { ChakraProvider } from "@chakra-ui/react"
-
-
-
-function MyApp({ Component, pageProps }) {
-  return(
-    <ChakraProvider>
-       <Provider store = {store}>
-         <Component {...pageProps} />
-      </Provider>
-    </ChakraProvider>
-  )
-}
-
-export default MyApp
-
 // import { Provider } from "react-redux"
-// import { store } from '../redux/store'
-// import { persistStore } from 'redux-persist';
-// import { PersistGate } from 'redux-persist/integration/react';
+// import store from "../redux/store"
 // import { ChakraProvider } from "@chakra-ui/react"
 
-// let persistor = persistStore(store);
 
 
 // function MyApp({ Component, pageProps }) {
 //   return(
-//       <ChakraProvider>
-//         <Provider store = {store}>
-//         <PersistGate persistor={persistor}>
+//     <ChakraProvider>
+//        <Provider store = {store}>
 //          <Component {...pageProps} />
-//         </PersistGate>
 //       </Provider>
-//       </ChakraProvider>
-      
+//     </ChakraProvider>
 //   )
 // }
 
 // export default MyApp
+
+import { Provider } from "react-redux"
+import { store } from '../redux/store'
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ChakraProvider } from "@chakra-ui/react"
+
+let persistor = persistStore(store);
+
+
+function MyApp({ Component, pageProps }) {
+  return(
+      <ChakraProvider>
+        <Provider store = {store}>
+        <PersistGate persistor={persistor}>
+         <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+      </ChakraProvider>
+      
+  )
+}
+
+export default MyApp
